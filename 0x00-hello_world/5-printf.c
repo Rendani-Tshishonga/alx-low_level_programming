99e10cf289d4e6cd8d0092c323c14a1c9ca527f1
Rendani Tshishonga <rendanijunior20@gmail.com>
2023-02-16 15:51:16 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints with printf
+ * Return: 0
+ * compile using gcc -Wall
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
