e63b38f4502e201d20361c980c6cbde7b195dfd0
Rendani Tshishonga <rendanijunior20@gmail.com>
2023-02-20 03:05:25 -0800

    Numbers

---
+++ 0x01-variable_if_else_while/5-print_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers with a base of 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d\n", num);
+	return (0);
+}
