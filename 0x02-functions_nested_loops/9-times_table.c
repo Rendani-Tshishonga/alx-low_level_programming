9c8736ce76c712a3a07dead17f3a2f02d3c2a448
Rendani Tshishonga <rendanijunior20@gmail.com>
2023-02-22 13:10:13 -0800

    _putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,18 @@
 
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int x, y;
+	int result;
 
-	for (a = 0; a <= 9; a++)
+	result = x * y;
+	for (x = 0; x <= 9; x++)
 	{
-	for (b = 0; b <= 9; b++)
+	for (y = 0; y <= 9; y++)
 	{
-	c = a * b;
-	if (c > 9)
-	{
-	d = c % 10;
-	e = (c - d) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(e + '0');
-	_putchar(d + '0');
-	}
-	else
-	if (b != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(c + '0');
-	}
+		_putchar(44);
+		_putchar(32);
+		_putchar(result + '0');
 	}
 	_putchar('\n');
+	}
 }
