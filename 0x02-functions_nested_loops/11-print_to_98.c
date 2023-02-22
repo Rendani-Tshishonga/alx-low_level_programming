ee3ae6bd0548c98971154c832327ec1e1f3673e2
Rendani Tshishonga <rendanijunior20@gmail.com>
2023-02-22 14:30:14 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,36 @@
 
 void print_to_98(int n)
 {
-	for (n = 1; n <= 98; n++)
+	if (n <= 98)
 	{
-		printf("%d", n);
-	}
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
 	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
 }
