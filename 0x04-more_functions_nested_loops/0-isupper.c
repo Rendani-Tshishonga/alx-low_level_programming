8caee638cf5b31a4df3a47b9c81371429b992e78
Rendani Tshishonga <rendanijunior20@gmail.com>
2023-02-23 05:09:22 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Checks for uppercase character
+ * @c: stores Uppercase Character
+ * Return: return 1 or return 0
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+write a function that checks for a digit 
