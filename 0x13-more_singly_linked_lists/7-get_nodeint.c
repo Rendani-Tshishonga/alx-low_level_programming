4f2981c071fd324e12a845fc5aec0ef0f2228481
Rendani Tshishonga <rendanijunior20@gmail.com>
2023-04-05 09:09:46 -0700

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
-	while (temp != NULL && i <= index)
+	while (temp != NULL && i < index)
 	{
 		i++;
 		temp = temp->next;
